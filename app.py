538eea5043b014e987977f46a3ccc71b4a22b70d
olive-su <1466su@naver.com>
2022-10-25 13:49:36 +0900

    Config: db settings

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask, render_template, jsonify, request
 from bson import ObjectId
 app = Flask(__name__)
+from dotenv import dotenv_values
 
 from pymongo import MongoClient
 
-client = MongoClient('localhost',
-                  username='test',
-                 password='test')
+config = dotenv_values(".env")
+client = MongoClient(config['HOST'],
+                  username=config['USERNAME'],
+                 password=config['PASSWORD'])
 db = client.dbjungle
 headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
 
