8bb0c74b5139b8c8932655f82e10a34ca355b822
whssodi <whssodi@gmail.com>
2022-10-26 14:34:20 +0900

    party cancel 카드 수정

--- app.py
+++ app.py
@@ -255,5 +255,6 @@ def party_cancel():
         db.party.update_one( { "_id" : object_id }, { "$set": { "state" : "0" } } );
     return jsonify({'result' : 'success'}) 
 
+
 if __name__ == '__main__':
    app.run('0.0.0.0',port=5000,debug=True)
